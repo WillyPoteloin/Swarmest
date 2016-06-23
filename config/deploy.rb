9121cdb68da40ee6a0b3386c2df30c8397a9dacc
willy <willypoteloin@yahoo.fr>
2016-06-24 00:29:14 +0200

    remove webpack as dependencies in package.json + change capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,7 +40,7 @@ namespace :deploy do
     desc "Install npm dependencies and build application"
     task :npm_build do
         on roles(:web) do |host|
-            execute "cd #{release_path} && npm install --production && npm run build:prod"
+            execute "cd #{release_path} && npm install && npm run build:prod"
             info "Installing npm dependencies and building the app"
         end
     end
