79209f16773536eae069ca53fc010e597802fdea
willy <willypoteloin@yahoo.fr>
2016-09-10 17:59:11 +0200

    change app state to an immutable state

--- src/reducers/tags.js
+++ src/reducers/tags.js
@@ -28,9 +28,10 @@ const tags = (state = initialState, action) => {
         case actionTypes.REMOVE_TAG:
 			tags = state.get('items')
 			index = tags.findIndex((elem) => {
-				return (elem.id == action.id) ? true : false
+				return (elem.get('id') == action.id) ? true : false
 			})
 			if(index != -1) tags = tags.splice(index, 1)
+            
             return state.set('items', tags)
         case actionTypes.SELECT_TAG:
 			selected_tags = state.get('selected_items')
@@ -38,6 +39,7 @@ const tags = (state = initialState, action) => {
 				return (id == action.id) ? true : false
 			})
 			if(index == -1) selected_tags = selected_tags.push(action.id)
+
             return state.set('selected_items', selected_tags)
         case actionTypes.UNSELECT_TAG:
 			selected_tags = state.get('selected_items')
@@ -45,17 +47,18 @@ const tags = (state = initialState, action) => {
 				return (id == action.id) ? true : false
 			})
 			if(index != -1) selected_tags = selected_tags.splice(index, 1)
+
             return state.set('selected_items', selected_tags)
         case actionTypes.FILTER_TAGS:
 			filtered_tags = (action.value) ? state.get('items') : null
 			if(filtered_tags) {
 				const regexp = new RegExp(action.value, 'i')
 				filtered_tags = filtered_tags.filter((tag) => {
-					return (regexp.test(tag.title)) ? true : false
+					return (regexp.test(tag.get('title'))) ? true : false
 				});
 			}
-            const new_state = state.set('filtered_items', filtered_tags)
-            return new_state.set('filter_value', action.value)
+
+            return state.set('filtered_items', filtered_tags).set('filter_value', action.value)
         default:
             return state
     }
